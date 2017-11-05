51b9cec4725a82d513f79bbe92e9b7913bf5fc19
D Blalock <dwb4ke@virginia.edu>
2017-11-04 21:54:03 -0400

    add inplace version of delta decode

--- cpp/Compress/delta.cpp
+++ cpp/Compress/delta.cpp
@@ -58,9 +58,9 @@ uint32_t encode_delta_rowmajor(const uint8_t* src, uint32_t len, int8_t* dest,
         orig_dest = dest; // NOTE: we don't do this in any other function
     }
 
-    printf("-------- compression (len = %lld, ndims = %d)\n", (int64_t)len, ndims);
-    // printf("saw original data:\n"); dump_bytes(src, len, ndims);
-    printf("saw original data:\n"); dump_bytes(src, ndims * 8, ndims);
+    // printf("-------- compression (len = %lld, ndims = %d)\n", (int64_t)len, ndims);
+    // // printf("saw original data:\n"); dump_bytes(src, len, ndims);
+    // printf("saw original data:\n"); dump_bytes(src, ndims * 8, ndims);
 
     // nblocks = 0; // TODO rm
 
@@ -75,7 +75,7 @@ uint32_t encode_delta_rowmajor(const uint8_t* src, uint32_t len, int8_t* dest,
     // printf("overrun ndims: %d\n", overrun_ndims);
 
     // ensure we don't write past the end of the output
-    uint16_t overrun_ndims = ndims % vector_sz;
+    uint16_t overrun_ndims = vector_sz - (ndims % vector_sz);
     uint32_t trailing_nelements = len % block_sz_elems;
     if (nblocks > 1 && overrun_ndims > trailing_nelements) { nblocks -= 1; }
     // if (nblocks > 1) { nblocks -= 1; }
@@ -94,6 +94,8 @@ uint32_t encode_delta_rowmajor(const uint8_t* src, uint32_t len, int8_t* dest,
                 __m256i vals = _mm256_loadu_si256((__m256i*)in_ptr);
                 __m256i vdeltas = _mm256_sub_epi8(vals, prev_vals);
                 _mm256_storeu_si256((__m256i*)out_ptr, vdeltas);
+                // hacky move instruction
+                // prev_vals = _mm256_xor_si256(vals, _mm256_setzero_si256());
                 prev_vals = vals;
             }
             _mm256_storeu_si256((__m256i*)(prev_vals_ptr), prev_vals);
@@ -125,6 +127,7 @@ uint32_t decode_delta_rowmajor(const int8_t* src, uint32_t len, uint8_t* dest,
     static const uint8_t vector_sz = 32;
     static const uint8_t block_sz = 8;
     uint8_t* orig_dest = dest;
+    const int8_t* orig_src = src;
 
     uint32_t block_sz_elems = block_sz * ndims;
     uint32_t nrows = len / ndims;
@@ -134,46 +137,44 @@ uint32_t decode_delta_rowmajor(const int8_t* src, uint32_t len, uint8_t* dest,
 
     uint8_t* prev_vals_ar = (uint8_t*)calloc(vector_sz, nvectors);
 
-    printf("-------- decompression (len = %lld, ndims = %d)\n", (int64_t)len, ndims);
-    printf("saw compressed data:\n");
+    // printf("-------- decompression (len = %lld, ndims = %d)\n", (int64_t)len, ndims);
+    // printf("saw compressed data:\n");
     // dump_bytes(src, len, ndims);
-    dump_bytes(src, ndims * 8, ndims);
-
-    // nblocks = 0; // TODO rm
-
-
-    // uint16_t overrun_ndims = ndims % vector_sz;
-    // uint32_t npad_bytes = overrun_ndims * block_sz;
-    // uint32_t block_sz_elems = block_sz * ndims;
-    // uint32_t npad_blocks = (npad_bytes / block_sz_elems) + ((npad_bytes % block_sz_elems) > 0);
-    // nblocks -= MIN(nblocks, npad_blocks);
-
-    // printf("overrun ndims: %d\n", overrun_ndims);
-    // if (len < vector_sz * block_sz) { nblocks = 0; }
+    // dump_bytes(src, ndims * 8, ndims);
 
     // ensure we don't write past the end of the output
-
-    uint16_t overrun_ndims = ndims % vector_sz;
+    uint16_t overrun_ndims = vector_sz - (ndims % vector_sz);
     uint32_t trailing_nelements = len % block_sz_elems;
     if (nblocks > 1 && overrun_ndims > trailing_nelements) { nblocks -= 1; }
 
-    // printf("using nblocks, nvectors: %d, %d\n", nblocks, nvectors);
-
     // nblocks = 0;
+    // printf("using nblocks: %d\n", nblocks);
 
     for (uint32_t b = 0; b < nblocks; b++) { // for each block
+        const int8_t* block_in_ptr = src + (nvectors - 1) * vector_sz;
+        uint8_t* block_out_ptr = dest + (nvectors - 1) * vector_sz;
         for (int32_t v = nvectors - 1; v >= 0; v--) { // for each stripe
-            __m256i* prev_vals_ptr = (__m256i*)(prev_vals_ar + v * vector_sz);
+            __m256i* prev_vals_ptr = (__m256i*)(&prev_vals_ar[0] + v * vector_sz);
             __m256i prev_vals = _mm256_loadu_si256(prev_vals_ptr);
+            const int8_t* in_ptr = block_in_ptr;
+            uint8_t* out_ptr = block_out_ptr;
             for (uint8_t i = 0; i < block_sz; i++) {
-                const int8_t* in_ptr = src + i * ndims + v * vector_sz;
-                uint8_t* out_ptr = dest + i * ndims + v * vector_sz;
+                // const int8_t* in_ptr = src + i * ndims + v * vector_sz;
+                // uint8_t* out_ptr = dest + i * ndims + v * vector_sz;
+                // const int8_t* in_ptr = block_in_ptr + i * ndims;
+                // uint8_t* out_ptr = block_out_ptr + i * ndims;
+                // if (b == 0) { printf("---- i = %d (offset %d)\n", i, (int)(in_ptr - orig_src)); }
                 __m256i errs = _mm256_loadu_si256((__m256i*)in_ptr);
                 __m256i vals = _mm256_add_epi8(errs, prev_vals);
+                // if (b == 0) { printf("vals:     "); dump_m256i(vals); }
                 _mm256_storeu_si256((__m256i*)out_ptr, vals);
                 prev_vals = vals;
+                in_ptr += ndims;
+                out_ptr += ndims;
             }
             _mm256_storeu_si256((__m256i*)(prev_vals_ptr), prev_vals);
+            block_in_ptr -= vector_sz;
+            block_out_ptr -= vector_sz;
         } // for each vector
         src += block_sz_elems;
         dest += block_sz_elems;
@@ -188,17 +189,38 @@ uint32_t decode_delta_rowmajor(const int8_t* src, uint32_t len, uint8_t* dest,
     }
     // printf("copying trailing %d bytes\n", (int)((orig_dest + len) - dest));
     for (; dest < (orig_dest + len); ++dest) {
-        // printf("copying element %d\n", (int)(dest - orig_dest));
         *dest = *src + *(dest - ndims);
         src++;
     }
 
-    printf("reconstructed input:\n");
-    dump_bytes(orig_dest, ndims * 8, ndims);
+    // printf("reconstructed input:\n");
+    // dump_bytes(orig_dest, ndims * 8, ndims);
 
     return len;
 }
 
+uint32_t decode_delta_rowmajor_inplace(uint8_t* buff, uint32_t len,
+                                       uint16_t ndims)
+{
+    // TODO might go a bit faster if we batch the copies
+    //
+    // static const uint8_t vector_sz = 32;
+    // static const uint8_t block_sz = 8;
+    //
+    // uint32_t block_sz_elems = block_sz * ndims;
+    // uint32_t nrows = len / ndims;
+    // uint32_t nblocks = nrows / block_sz;
+    //
+    // static const uint8_t batch_nblocks = 8;
+    // uint32_t batch_size_elems = block_sz_elems * batch_nblocks;
+    // uint8_t* tmp = (uint8_t*)malloc(batch_size_elems + vector_sz);
+    // decode_delta_rowmajor((int8_t*)buff, len, tmp, ndims);
+
+    uint8_t* tmp = (uint8_t*)malloc(len);
+    uint32_t sz = decode_delta_rowmajor((int8_t*)buff, len, tmp, ndims);
+    memcpy(buff, tmp, sz);
+    return sz;
+}
 
 uint32_t decode_delta_rowmajor(const int8_t* src, uint8_t* dest) {
     uint32_t len = *(uint32_t*)src;
--- cpp/Compress/delta.h
+++ cpp/Compress/delta.h
@@ -15,6 +15,8 @@ uint32_t encode_delta_rowmajor(const uint8_t* src, uint32_t len, int8_t* dest,
                            uint16_t ndims, bool write_size=true);
 uint32_t decode_delta_rowmajor(const int8_t* src, uint32_t len, uint8_t* dest,
                            uint16_t ndims);
+uint32_t decode_delta_rowmajor_inplace(uint8_t* buff, uint32_t len,
+                                       uint16_t ndims);
 uint32_t decode_delta_rowmajor(const int8_t* src, uint8_t* dest);
 
 #endif /* delta_hpp */
