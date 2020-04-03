201fef7c4e5e1f6d46f5ca4764ec447a3148c1cf
D Blalock <dwb4ke@virginia.edu>
2020-04-02 20:10:32 -0400

    add metadata read+write funcs for when 1d case is known a priori

--- cpp/Compress/format.cpp
+++ cpp/Compress/format.cpp
@@ -35,6 +35,13 @@ uint16_t read_metadata_simple_8b(const int8_t* src, uint16_t* p_ndims,
     return read_metadata_simple(src, p_ndims, p_len);
 }
 
+uint16_t write_metadata_simple1d_8b(int8_t* orig_dest, uint32_t len) {
+    return write_metadata_simple1d(orig_dest, len);
+}
+uint16_t read_metadata_simple1d_8b(const int8_t* src, uint32_t* p_len) {
+    return read_metadata_simple1d(src, p_len);
+}
+
 // ------------------------------------------------ 16b wrappers
 
 uint16_t write_metadata_rle_16b(int16_t* orig_dest, uint16_t ndims,
@@ -57,6 +64,13 @@ uint16_t read_metadata_simple_16b(const int16_t* src, uint16_t* p_ndims,
     uint32_t* p_len)
 {
     return read_metadata_simple(src, p_ndims, p_len);
+}
+
+uint16_t write_metadata_simple1d_16b(int16_t* orig_dest, uint32_t len) {
+    return write_metadata_simple1d(orig_dest, len);
+}
+uint16_t read_metadata_simple1d_16b(const int16_t* src, uint32_t* p_len) {
+    return read_metadata_simple1d(src, p_len);
 }
 
 // #undef DIV_ROUND_UP
--- cpp/Compress/format.h
+++ cpp/Compress/format.h
@@ -30,6 +30,7 @@
 
 #define kMetaDataLenBytesRle 8
 #define kMetaDataLenBytesSimple 6
+#define kMetaDataLenBytesSimple1d 4
 
 template<typename int_t>
 static inline uint16_t write_metadata_rle(int_t* orig_dest, uint16_t ndims,
@@ -84,30 +85,45 @@ static inline uint16_t read_metadata_simple(const int_t* src, uint16_t* p_ndims,
     // return (len_nbytes / elem_sz) + ((len_nbytes % elem_sz) > 0);
 }
 
+template<typename int_t>
+static inline uint16_t write_metadata_simple1d(int_t* dest, uint32_t len)
+{
+    *(uint32_t*)dest = len;
+    return DIV_ROUND_UP(kMetaDataLenBytesSimple1d, sizeof(int_t));
+}
+template<typename int_t>
+static inline uint16_t read_metadata_simple1d(const int_t* src, uint32_t* p_len)
+{
+    *p_len = *(uint32_t*)src;
+    return DIV_ROUND_UP(kMetaDataLenBytesSimple1d, sizeof(int_t));
+}
+
 // ------------------------------------------------ 8b wrappers
 
 uint16_t write_metadata_rle_8b(int8_t* dest, uint16_t ndims, uint32_t ngroups,
     uint16_t remaining_len);
-
 uint16_t read_metadata_rle_8b(const int8_t* src, uint16_t* p_ndims,
     uint32_t* p_ngroups, uint16_t* p_remaining_len);
 
 uint16_t write_metadata_simple_8b(int8_t* dest, uint16_t ndims, uint32_t len);
-
 uint16_t read_metadata_simple_8b(const int8_t* src, uint16_t* p_ndims,
     uint32_t* p_len);
+
+uint16_t write_metadata_simple1d_8b(int8_t* dest, uint32_t len);
+uint16_t read_metadata_simple1d_8b(const int8_t* src, uint32_t* p_len);
 
 // ------------------------------------------------ 16b wrappers
 
 uint16_t write_metadata_rle_16b(int8_t* dest, uint16_t ndims, uint32_t ngroups,
     uint16_t remaining_len);
-
 uint16_t read_metadata_rle_16b(const int8_t* src, uint16_t* p_ndims,
     uint64_t* p_ngroups, uint16_t* p_remaining_len);
 
 uint16_t write_metadata_simple_16b(int16_t* dest, uint16_t ndims, uint32_t len);
-
 uint16_t read_metadata_simple_16b(const int16_t* src, uint16_t* p_ndims,
     uint32_t* p_len);
+
+uint16_t write_metadata_simple1d_16b(int16_t* dest, uint32_t len);
+uint16_t read_metadata_simple1d_16b(const int16_t* src, uint32_t* p_len);
 
 #endif /* format_hpp */
